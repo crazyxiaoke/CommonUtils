4aefa095a0535018931ed9ff6cf4da59084a59c5
xiaoke9120 <459322094@qq.com>
2019-03-07 10:57:40 +0800

    添加网络工具类

---
+++ commonutils/src/main/java/com/hz/zxk/commonutils/utils/NetworkUtils.java
@@ -0,0 +1,40 @@
+package com.hz.zxk.commonutils.utils;
+
+/**
+ * 　　┏┓　　　　┏┓
+ * 　┏┛┻━━━━┛┻┓
+ * 　┃　　　　　　　　┃
+ * 　┃　　　━　　　　┃
+ * 　┃　┳┛　┗┳　　┃
+ * 　┃　　　　　　　　┃
+ * 　┃　　　┻　　　　┃
+ * 　┃　　　　　　　　┃
+ * 　┗━━┓　　　┏━┛
+ * 　　　　┃　　　┃　　　神兽保佑
+ * 　　　　┃　　　┃　　　代码无BUG！
+ * 　　　　┃　　　┗━━━┓
+ * 　　　　┃　　　　　　　┣┓
+ * 　　　　┃　　　　　　　┏┛
+ * 　　　　┗┓┓┏━┳┓┏┛
+ * 　　　　　┃┫┫　┃┫┫
+ * <p>
+ * Created by zxk on 19-3-4.
+ */
+public class NetworkUtils {
+    /**
+     * 字节转换
+     *
+     * @param b
+     * @return
+     */
+    public static String networkSpeed(long b) {
+        if (b < 1024) {
+            return b + "Byte";
+        } else if (b < 1024 * 1024) {
+            return b / 1024 + "KB";
+        } else if (b < 1024 * 1024 * 1024) {
+            return ((float) (b * 100 / 1024 / 1024 * 1024)) / 100 + "MB";
+        }
+        return "0Byte";
+    }
+}
---
+++ commonutils/src/main/java/com/hz/zxk/commonutils/utils/StringUtils.java
@@ -0,0 +1,60 @@
+package com.hz.zxk.commonutils.utils;
+
+/**
+ * 　　┏┓　　　　┏┓
+ * 　┏┛┻━━━━┛┻┓
+ * 　┃　　　　　　　　┃
+ * 　┃　　　━　　　　┃
+ * 　┃　┳┛　┗┳　　┃
+ * 　┃　　　　　　　　┃
+ * 　┃　　　┻　　　　┃
+ * 　┃　　　　　　　　┃
+ * 　┗━━┓　　　┏━┛
+ * 　　　　┃　　　┃　　　神兽保佑
+ * 　　　　┃　　　┃　　　代码无BUG！
+ * 　　　　┃　　　┗━━━┓
+ * 　　　　┃　　　　　　　┣┓
+ * 　　　　┃　　　　　　　┏┛
+ * 　　　　┗┓┓┏━┳┓┏┛
+ * 　　　　　┃┫┫　┃┫┫
+ * <p>
+ * Created by zxk on 19-3-7.
+ */
+public class StringUtils {
+
+    /**
+     * 判断字符串是否为空
+     *
+     * @param str
+     * @return
+     */
+    public static boolean isEmpty(String str) {
+        if (str == null || str.trim().length() == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 判断字符串是否不为空
+     *
+     * @param str
+     * @return
+     */
+    public static boolean isNotEmpty(String str) {
+        return !isEmpty(str);
+    }
+
+    /**
+     * 从路径中获取文件名
+     *
+     * @param url
+     * @return
+     */
+    public static String getFileNameFromUrl(String url) {
+        if (isNotEmpty(url)) {
+            return url.substring(url.lastIndexOf("/"), url.length());
+        }
+        return "";
+    }
+}
